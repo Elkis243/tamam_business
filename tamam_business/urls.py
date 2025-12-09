0ef64b4733528dc69e523742e1f53c70d68cfba4
Elkis243 <elielubama@gmail.com>
2025-12-10 00:50:15 +0100

    Update favicon handling and URL configurations: Remove old favicon.ico file, implement redirection for favicon requests, and adjust favicon links in base.html to point to the new directory structure for improved organization and accessibility.

--- tamam_business/urls.py
+++ tamam_business/urls.py
@@ -14,12 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 from app import error_handlers
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path("admin/", admin.site.urls),
+    # Redirection pour favicon.ico
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url="/static/images/favicon/favicon.ico", permanent=True),
+    ),
+    path("", include("app.urls")),
 ]
 
 # Custom error handlers
